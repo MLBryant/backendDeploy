3a24f21505110f5a7c5e688328f1600e613723fc
Mason Bryant <mlbryant6@gmail.com>
2021-11-17 12:19:07 -0600

    first commit

---
+++ client/main.js
@@ -0,0 +1,89 @@
+const addQuote = document.querySelector('#quoteInput')
+const addQuoteForm = document.querySelector('#quoteInputForm')
+const quoteDropdown = document.getElementsByName('quote')
+const editQuote = document.querySelector('#quoteEdit')
+const editedQuote = document.querySelector('#editedQuote')
+const editQuoteForm = document.querySelector('#quoteEditForm')
+const deleteQuote = document.querySelector('#quoteDelete')
+const deleteQuoteForm = document.querySelector('#quoteDeleteForm')
+const quoteList = document.querySelector('#quoteSection')
+
+const inputQuote = event => {
+    event.preventDefault()
+    let newQuote = {
+        quote: addQuote.value
+    }
+    axios.post("http://localhost:4000/api/quote/", newQuote)
+    .then(res => {
+        console.log(res.data);
+        getQuotes()
+    })
+    addQuote.value = ''
+}
+
+const changeQuote = event => {
+    event.preventDefault()
+    if (+editQuote.value > 0) {
+        let newQuote = {
+            id: +editQuote.value,
+            quote: editedQuote.value
+        }
+        axios.put(`http://localhost:4000/api/quote/${newQuote.id}`, newQuote)
+        .then(res => {
+            console.log(res.data);
+            getQuotes()
+        })
+    } else {
+        alert('Please select a quote')
+    }
+    editedQuote.value = ''
+}
+
+const removeQuote = event => {
+    event.preventDefault()
+    if (+deleteQuote.value > 0) {
+        let id = +deleteQuote.value
+        axios.delete(`http://localhost:4000/api/quote/${id}`)
+        .then(res => {
+            console.log(res.data);
+            getQuotes()
+        })
+    } else {
+        alert('Please select a quote')
+    }
+}
+const getQuotes = () => {
+    
+    axios.get('http://localhost:4000/api/quote')
+    .then(res => {
+        for (x = 0; x < quoteDropdown.length; x++) {
+            quoteDropdown[x].innerHTML = ''
+            let nullOption = document.createElement('option')
+            nullOption.value = 0
+            nullOption.text = 'Select quote'
+            quoteDropdown[x].appendChild(nullOption)
+        }
+        quoteList.innerHTML = '';
+        for(i = 0; i < res.data.length; i++) {
+            let addedQuote = document.createElement('h3')
+            addedQuote.textContent = `"${res.data[i].quote}"`
+            quoteList.appendChild(addedQuote)
+            for(j = 0; j < quoteDropdown.length; j++) {
+                let addedQuoteDropdown = document.createElement('option')
+                addedQuoteDropdown.value = +res.data[i].id
+                if (res.data[i].quote.length > 20) {
+                    addedQuoteDropdown.text = res.data[i].quote.slice(0, 20) + '...'
+                } else {
+                    addedQuoteDropdown.text = res.data[i].quote
+                }
+                quoteDropdown[j].appendChild(addedQuoteDropdown, 0)
+            }
+        }
+    }) 
+}
+
+addQuoteForm.addEventListener('submit', inputQuote)
+editQuoteForm.addEventListener('submit', changeQuote)
+deleteQuoteForm.addEventListener('submit', removeQuote)
+
+getQuotes()
---
+++ server/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+const path = require('path')
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+let quotes = [{'id': 1,'quote':'We shine bright so that others may shine brighter.'}, {'id': 2,'quote':'Success is not final, failure is not fatal: it is the courage to continue that counts.'}, {'id': 3,'quote':'Life is like riding a bicycle. To keep your balance, you must keep moving.'}]
+quoteId = 4
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+
+app.get('/api/fortune', (req, res) => {
+  const fortunes = ['Listen not to vain words of empty tongue.', 'Like the river flow into the sea. Something are just meant to be.', 'Po Says: Pandas like eating bamboo, but I prefer mine dipped in chocolate.', 'Society prepares the crime; the criminal commits it.', 'The first man gets the oyster, the second man gets the shell.', 'Tonight you will be blinded by passion.', 'Fortune Not Found: Abort, Retry, Ignore?', 'Feeding a cow with roses does not get extra appreciation.', 'Do not demand for someone’s soul if you already got his heart.', 'Any decision you have to make tomorrow is a good decision.']
+  let randomFortune = fortunes[Math.floor(Math.random() * 10)]
+  res.status(200).send(randomFortune)
+})
+
+app.get(`/api/quote`, (req, res) => res.status(200).send(quotes))
+app.post(`/api/quote`, (req, res) => {
+  let {quote} = req.body
+  let quoteToAdd = {
+    id: quoteId,
+    quote
+  }
+  quotes.push(quoteToAdd)
+  res.status(200).send(quoteToAdd)
+  quoteId++
+})
+app.put(`/api/quote/:id`, (req, res) => {
+  let {id} = req.params
+  let {quote} = req.body
+  let index = quotes.findIndex(elem => +elem.id === +req.params.id)
+  quotes[index].quote = quote
+  res.status(200).send(quotes[+id])
+})
+app.delete(`/api/quote/:id`, (req, res) => {
+  let {id} = req.params
+  let index = quotes.findIndex(elem => +elem.id === +req.params.id)
+  removedQuote = quotes.splice(index, 1)
+  res.status(200).send(removedQuote)
+})
+
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`Server running on ${port}`));
