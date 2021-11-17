8e179095a93e1518377bedc185d0c31f9e59d92d
Mason Bryant <mlbryant6@gmail.com>
2021-11-17 16:05:20 -0600

    moved button script to js file

--- client/main.js
+++ client/main.js
@@ -8,6 +8,22 @@ const deleteQuote = document.querySelector('#quoteDelete')
 const deleteQuoteForm = document.querySelector('#quoteDeleteForm')
 const quoteList = document.querySelector('#quoteSection')
 
+document.getElementById("fortuneButton").onclick = function () {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        });
+  };
+  
+  document.getElementById("complimentButton").onclick = function () {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        });
+  };
+
 const inputQuote = event => {
     event.preventDefault()
     let newQuote = {
